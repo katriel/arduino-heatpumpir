ca1e5db756699a4d168ba20e2210807896cbb013
Katriel Traum <katriel@traum.org.il>
2026-09-16 21:07:04 +0300

    Support YB1FA

--- GreeHeatpumpIR.h
+++ GreeHeatpumpIR.h
@@ -131,12 +131,15 @@ class GreeYB1FAHeatpumpIR : public GreeHeatpumpIR
 {
   public:
     GreeYB1FAHeatpumpIR();
-    void generateCommand(uint8_t * buffer,
-                         uint8_t powerMode, uint8_t operatingMode,
-                         uint8_t fanSpeed, uint8_t temperature,
-                         uint8_t swingV, uint8_t swingH,
-                         bool turboMode, bool iFeelMode) override;
-    void calculateChecksum(uint8_t * buffer) override;
+
+    protected:
+      virtual void generateCommand(uint8_t * buffer,
+              uint8_t powerMode, uint8_t operatingMode,
+              uint8_t fanSpeed, uint8_t temperature,
+              uint8_t swingV, uint8_t swingH,
+              bool turboMode, bool iFeelMode) override;
+    
+    virtual void calculateChecksum(uint8_t * buffer) override;
 };
 
 class GreeGenericHeatpumpIR : public GreeHeatpumpIR
